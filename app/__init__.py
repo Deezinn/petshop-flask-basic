3b303b7e3d90ca0a53a25c4757df802cfb42fc33
Deezinn <developer.andrelz@gmail.com>
2025-04-08 23:14:13 -0300

    add '/' route

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from flask_migrate import Migrate
 from .model import configure as config_db
 from .serializer import configure as config_ma
 
+@app.route("/")
+def index():
+    return "API Flask está rodando na Azure! CRUD de um petshop"
+
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/pets.db'
